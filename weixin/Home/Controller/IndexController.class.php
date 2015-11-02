7a0cdf9449858928ef90c22319a7073d7a18d89f
git03@lnamphp.com <git03@lnamphp.com>
2015-11-02 22:11:47 +0800

    1
    1

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -29,5 +29,60 @@ class IndexController extends Controller
         }
     }
     //接收事件并推送
-    
+    public function reponseMse(){
+        //接收
+        $postAtt = $GLOBALS('HTTP_RAW_POST_DATA ');
+        //处理消息类型 并设置回复类型和内容
+        $postAbj = simplexml_load_string($postArr);
+//                <xml>
+//        <ToUserName><![CDATA[toUser]]></ToUserName>
+//        <FromUserName><![CDATA[FromUser]]></FromUserName>
+//        <CreateTime>123456789</CreateTime>
+//        <MsgType><![CDATA[event]]></MsgType>
+//        <Event><![CDATA[subscribe]]></Event>
+//        </xml>
+        $postAbj->ToUserName=''; 
+        $postAbj->FromUserName='';
+        $postAbj->CreateTime='';
+        $postAbj->MsgType='';    
+        $postAbj->Event='';
+        if(strtolower($postAbj->MsgType)=='event'){
+            if(strtolower($postAbj->Event=='subscribe')){
+                //回复用户消息
+                $toUser = $postAbj->FormUserName;
+                $formUser = $postAbj->toUserName;
+                $time = $time();
+                $Msgtype='text';
+                $Content='欢迎你，亲';
+                $template = "<xml>
+                <ToUserName><![CDATA[%s]]></ToUserName>
+                <FromUserName><![CDATA[%s]]></FromUserName>
+                <CreateTime>%s</CreateTime>
+                <MsgType><![CDATA[%s]]></MsgType>
+                <Event><![CDATA[%s]]></Event>
+                </xml>";
+                $info = spintf($toUser,$formUser,$time,$Msgtype,$Content);
+                    echo $info;
+                }
+        }
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
