1cbf532439226d99bfa89f941677494c46e7c34d
git03@lnamphp.com <git03@lnamphp.com>
2015-11-02 14:17:47 +0800

    del
    删除   index.txt  1.txt

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 define('APP_DEBUG', true);
 
 // 定义应用目录
-define('APP_PATH', './Application/');
+define('APP_PATH', './weixin/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
