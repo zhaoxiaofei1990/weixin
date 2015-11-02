3efa4490a872938235c7ebf45d64195f9d386824
git03@lnamphp.com <git03@lnamphp.com>
2015-11-02 22:30:22 +0800

    1
    1

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -10,23 +10,26 @@ class IndexController extends Controller
     public function index()
     {
         //获得参数
-        $nonce = $_GET['nonce'];
-        $token = 'test';
+   	//获得参数 signature nonce token timestamp echostr
+        $nonce     = $_GET['nonce'];
+        $token     = 'imooc';
         $timestamp = $_GET['timestamp'];
-        $echostr = $_GET['echostr'];
-        $signatrue = $_GET['signatrue'];
-        //形成数组 然后按字典排序
+        $echostr   = $_GET['echostr'];
+        $signature = $_GET['signature'];
+        //形成数组，然后按字典序排序
         $array = array();
-        $array = array($nonce,$timestamp,$token);
+        $array = array($nonce, $timestamp, $token);
         sort($array);
-        $str = sha1(implode($array));
-        if($str == $signatrue &&$echostr){
-            echo $echostr;
-            exit();
-        }  else {
-            $this->reponseMsg();
+        //拼接成字符串,sha1加密 ，然后与signature进行校验
+        $str = sha1( implode( $array ) );
+        if( $str  == $signature && $echostr ){
+                //第一次接入weixin api接口的时候
+                echo  $echostr;
+                exit;
+        }else{
+                $this->reponseMsg();
         }
-    }
+}
     //接收事件并推送
     public function reponseMse(){
         //接收
