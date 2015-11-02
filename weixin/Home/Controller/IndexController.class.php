7dc1041f5d99451efadec62bf897d7a67f5a58cf
git03@lnamphp.com <git03@lnamphp.com>
2015-11-02 22:44:53 +0800

    4
    5

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -7,66 +7,76 @@ class IndexController extends Controller
 {
     public function __construct() {
     }
-    public function index()
-    {
-        //获得参数
-        $nonce = $_GET['nonce'];
-        $token = 'test';
-        $timestamp = $_GET['timestamp'];
-        $echostr = $_GET['echostr'];
-        $signatrue = $_GET['signatrue'];
-        //形成数组 然后按字典排序
-        $array = array();
-        $array = array($nonce,$timestamp,$echostr,$signatrue);
-        sort($array);
-        //yong sha1方式加密
-        $str = sha1($array);
-        if($str == $signatrue &&$echostr){
-            echo $echostr;
-            exit();
-        }  else {
-            $this->reponseMsg();
-        }
-    }
-    //接收事件并推送
-    public function reponseMse(){
-        //接收
-        $postAtt = $GLOBALS('HTTP_RAW_POST_DATA ');
-        //处理消息类型 并设置回复类型和内容
-        $postAbj = simplexml_load_string($postArr);
-//                <xml>
-//        <ToUserName><![CDATA[toUser]]></ToUserName>
-//        <FromUserName><![CDATA[FromUser]]></FromUserName>
-//        <CreateTime>123456789</CreateTime>
-//        <MsgType><![CDATA[event]]></MsgType>
-//        <Event><![CDATA[subscribe]]></Event>
-//        </xml>
-        $postAbj->ToUserName=''; 
-        $postAbj->FromUserName='';
-        $postAbj->CreateTime='';
-        $postAbj->MsgType='';    
-        $postAbj->Event='';
-        if(strtolower($postAbj->MsgType)=='event'){
-            if(strtolower($postAbj->Event=='subscribe')){
-                //回复用户消息
-                $toUser = $postAbj->FormUserName;
-                $formUser = $postAbj->toUserName;
-                $time = $time();
-                $Msgtype='text';
-                $Content='欢迎你，亲';
-              $template = "<xml>
-                <ToUserName><![CDATA[%s]]></ToUserName>
-                <FromUserName><![CDATA[%s]]></FromUserName>
-                <CreateTime>%s</CreateTime>
-                <MsgType><![CDATA[%s]]></MsgType>
-                <Event><![CDATA[%s]]></Event>
-                </xml>";
-                $info = spintf($toUser,$formUser,$time,$Msgtype,$Content);
-                    echo $info;
-            }
-        }
+    public function index(){
+		//获得参数 signature nonce token timestamp echostr
+		$nonce     = $_GET['nonce'];
+		$token     = 'imooc';
+		$timestamp = $_GET['timestamp'];
+		$echostr   = $_GET['echostr'];
+		$signature = $_GET['signature'];
+		//形成数组，然后按字典序排序
+		$array = array();
+		$array = array($nonce, $timestamp, $token);
+		sort($array);
+		//拼接成字符串,sha1加密 ，然后与signature进行校验
+		$str = sha1( implode( $array ) );
+		if( $str  == $signature && $echostr ){
+			//第一次接入weixin api接口的时候
+			echo  $echostr;
+			exit;
+		}else{
+			$this->reponseMsg();
+		}
+	}
+	// 接收事件推送并回复
+	public function reponseMsg(){
+		//1.获取到微信推送过来post数据（xml格式）
+		$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+		//2.处理消息类型，并设置回复类型和内容
+		/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[FromUser]]></FromUserName>
+<CreateTime>123456789</CreateTime>
+<MsgType><![CDATA[event]]></MsgType>
+<Event><![CDATA[subscribe]]></Event>
+</xml>*/
+		$postObj = simplexml_load_string( $postArr );
+		//$postObj->ToUserName = '';
+		//$postObj->FromUserName = '';
+		//$postObj->CreateTime = '';
+		//$postObj->MsgType = '';
+		//$postObj->Event = '';
+		// gh_e79a177814ed
+		//判断该数据包是否是订阅的事件推送
+		if( strtolower( $postObj->MsgType) == 'event'){
+			//如果是关注 subscribe 事件
+			if( strtolower($postObj->Event == 'subscribe') ){
+				//回复用户消息(纯文本格式)	
+				$toUser   = $postObj->FromUserName;
+				$fromUser = $postObj->ToUserName;
+				$time     = time();
+				$msgType  =  'text';
+				$content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+				$template = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>";
+				$info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+				echo $info;
+/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[fromUser]]></FromUserName>
+<CreateTime>12345678</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[你好]]></Content>
+</xml>*/
+			
 
-    }
+			}
+		}
 }
 
 
