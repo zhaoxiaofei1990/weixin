6dbe166e8c3ff36750bdd8e053a0a088026ff554
git03@lnamphp.com <git03@lnamphp.com>
2015-11-03 14:07:23 +0800

    1
    1

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -26,29 +26,36 @@ class IndexController extends Controller {
             $this->reponseMsg();
         }
     }
-    public function reponseMsg(){
+
+    public function reponseMsg() {
         //获取微信的值
         $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
         $postObj = simplexml_load_string($postArr);
-        //判断该包是否是订阅推送事件
-        if(strtolower($postObj->msgType=='event')){
-            if(strtolower($postObj->Event)=='subscribe'){
-             //组装数据包
+        //$postObj->ToUserName = '';
+        //$postObj->FromUserName = '';
+        //$postObj->CreateTime = '';
+        //$postObj->MsgType = '';
+        //$postObj->Event = '';
+        // gh_e79a177814ed
+        //判断该数据包是否是订阅的事件推送
+        if (strtolower($postObj->MsgType) == 'event') {
+            //如果是关注 subscribe 事件
+            if (strtolower($postObj->Event == 'subscribe')) {
+                //回复用户消息(纯文本格式)	
                 $toUser = $postObj->FromUserName;
                 $fromUser = $postObj->ToUserName;
                 $time = time();
-                $msgType='text';
-                $content = '欢迎你,亲'.$postObj->FromUserName.'-'.$postObj->ToUserName;
-                $tmp = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Event><![CDATA[%s]]></Event>
-                        </xml>";
-                $info = srintf($tmp,$toUser,$fromUser,$time,$msgType,$content);
+                $msgType = 'text';
+                $content = '欢迎关注我们的微信公众账号' . $postObj->FromUserName . '-' . $postObj->ToUserName;
+                $template = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[%s]]></MsgType>
+                            <Content><![CDATA[%s]]></Content>
+                            </xml>";
+                $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
                 echo $info;
-                
             }
         }
     }
