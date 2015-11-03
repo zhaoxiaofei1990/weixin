75dae61f7923f069e1494328a8606f6d02b0c841
git03@lnamphp.com <git03@lnamphp.com>
2015-11-03 12:05:48 +0800

    2
    2

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -1,16 +1,20 @@
 <?php
-
 namespace Home\Controller;
 
 use Think\Controller;
 
-class IndexController extends Controller {
-
+class IndexController extends Controller
+{
     public function __construct() {
+<<<<<<< HEAD
         
     }
     public function Index(){
     
+=======
+    }
+    public function index(){
+>>>>>>> parent of 1db01b8... 11.3
 		//获得参数 signature nonce token timestamp echostr
 		$nonce     = $_GET['nonce'];
 		$token     = 'imooc';
@@ -19,7 +23,11 @@ class IndexController extends Controller {
 		$signature = $_GET['signature'];
 		//形成数组，然后按字典序排序
 		$array = array();
+<<<<<<< HEAD
 		$array = array($nonce, $timestamp, $token);
+=======
+		$array = array($nonce, $timestamp, $token,$signature);
+>>>>>>> parent of 1db01b8... 11.3
 		sort($array);
 		//拼接成字符串,sha1加密 ，然后与signature进行校验
 		$str = sha1( implode( $array ) );
@@ -27,8 +35,81 @@ class IndexController extends Controller {
 			//第一次接入weixin api接口的时候
 			echo  $echostr;
 			exit;
+<<<<<<< HEAD
 		}
 	
     }
+=======
+		}else{
+			$this->reponseMsg();
+		}
+	}
+	// 接收事件推送并回复
+	public function reponseMsg(){
+		//1.获取到微信推送过来post数据（xml格式）
+		$postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+		//2.处理消息类型，并设置回复类型和内容
+		/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[FromUser]]></FromUserName>
+<CreateTime>123456789</CreateTime>
+<MsgType><![CDATA[event]]></MsgType>
+<Event><![CDATA[subscribe]]></Event>
+</xml>*/
+		$postObj = simplexml_load_string( $postArr );
+		//$postObj->ToUserName = '';
+		//$postObj->FromUserName = '';
+		//$postObj->CreateTime = '';
+		//$postObj->MsgType = '';
+		//$postObj->Event = '';
+		// gh_e79a177814ed
+		//判断该数据包是否是订阅的事件推送
+		if( strtolower( $postObj->MsgType) == 'event'){
+			//如果是关注 subscribe 事件
+			if( strtolower($postObj->Event == 'subscribe') ){
+				//回复用户消息(纯文本格式)	
+				$toUser   = $postObj->FromUserName;
+				$fromUser = $postObj->ToUserName;
+				$time     = time();
+				$msgType  =  'text';
+				$content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+				$template = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>";
+				$info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+				echo $info;
+/*<xml>
+<ToUserName><![CDATA[toUser]]></ToUserName>
+<FromUserName><![CDATA[fromUser]]></FromUserName>
+<CreateTime>12345678</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA[你好]]></Content>
+</xml>*/
+			
+>>>>>>> parent of 1db01b8... 11.3
 
+			}
+		}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
