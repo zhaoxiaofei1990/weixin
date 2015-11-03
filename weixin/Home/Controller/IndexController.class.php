5ea9386e0096f09f9eb41a805709ac2a955a7720
git03@lnamphp.com <git03@lnamphp.com>
2015-11-03 16:48:25 +0800

    34
    34

--- weixin/Home/Controller/IndexController.class.php
+++ weixin/Home/Controller/IndexController.class.php
@@ -59,6 +59,19 @@ class IndexController extends Controller {
                     $content='哈喽';
                 break;    
             }
+            $tmplate = "<xml>
+                
+                        <ToUserName><![CDATA[%s]]></ToUserName>
+                        <FromUserName><![CDATA[%s]]></FrimUserName>
+                        <CreateTime>%s</CreateTime>
+                        <MsgType><![CDATA[%s]]></MsgType>
+                        <Content><![CDATA[%s]]></content>
+                    </xml>";
+            $fromUser = $postObj->ToUserName;
+            $toUser =$postObj->FromUserName;
+            $time = time();
+            $msgType ='text';
+            echo sprintf($template,$toUser,$fromUser,$time,$msgType,$content);
         }
     }
 
