0f5a49aef101e175f5451ecf2e129bbeba16d720
git03@lnamphp.com <git03@lnamphp.com>
2015-11-03 12:12:56 +0800

    5
    log

---
+++ weixin/Home/Controller/IndexController.class.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Home\Controller;
+
+use Think\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        echo '这是开发的微信公众平台';
+    }
+}
